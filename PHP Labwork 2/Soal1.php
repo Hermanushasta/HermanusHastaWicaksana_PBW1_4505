6e08b333842087426bcd0c5aa271e011ea8e0c1f
Hermanus Hasta <hasta.wicaksana@gmail.com>
2022-04-18 01:10:42 +0700

    update nama folder

---
+++ PHP Labwork 2/Soal1.php
@@ -0,0 +1,7 @@
+<?php 
+    $string = "D3 Rekayasa Perangkat Lunak Aplikasi";
+    
+    echo "<br>".substr($string, 3);
+    echo "<br>".substr($string, -8);
+    echo "<br>".substr($string, 3,24);
+?>
